7ee5df21e0803ebf16cb01ded9cba649ffedd9c0
Tyler Christensen <chr12061@byui.edu>
2017-11-10 17:15:30 -0700

    added prove 09

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/prove09', function(request, response) {
+  response.render('pages/prove09');
+});
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
